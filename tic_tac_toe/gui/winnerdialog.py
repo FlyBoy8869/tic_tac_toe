7d4a13f90aec68c91bfdda1e875821d647513080
FlyBoy8869 <charlescognato@gmail.com>
2021-04-09 11:35:17 -0400

    No window decorations on the winner dialog anymore

--- tic_tac_toe/gui/winnerdialog.py
+++ tic_tac_toe/gui/winnerdialog.py
@@ -1,4 +1,5 @@
 from PyQt5 import QtGui
+from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QDialog
 
 from .designerforms.winner_ui import Ui_Dialog
@@ -8,6 +9,7 @@ class WinnerDialog(QDialog, Ui_Dialog):
     def __init__(self, parent, pixmap, draw=False):
         super().__init__(parent)
         self.setupUi(self)
+        self.setWindowFlag(Qt.FramelessWindowHint)
 
         self.player.setPixmap(pixmap)
         if draw:
